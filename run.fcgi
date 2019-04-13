9eaf37ba7985f5bba507e81abc5d26eb1c942f3a
ankurmahesh <ankurmahesh@ocf.berkeley.edu>
2019-04-12 18:37:59 -0700

    setting password as environment variable

--- run.fcgi
+++ run.fcgi
@@ -1,6 +1,7 @@
 #!venv/bin/python
 from flup.server.fcgi import WSGIServer
 from weatherapplication import app
-
+from dotenv import load_dotenv
 if __name__ == '__main__':
+    load_dotenv(".env")
     WSGIServer(app).run()
--- weatherapplication/__init__.py
+++ weatherapplication/__init__.py
@@ -26,8 +26,7 @@ def get_data():
    sql_hostname = 'mysql'
    sql_username='ankurmahesh'
 
-   sql_password = ''
-   #sql_password = os.environ["SQL_PASSWORD"]
+   sql_password = os.environ["SQL_PASSWORD"]
 
    #with open("sql_password", "r") as f:
    #    sql_password = f.read().replace("\n", "")
